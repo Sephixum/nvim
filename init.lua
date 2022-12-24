12caee2eee80809aeb66a765ba2d19aabfb21e42
Sephixum <emotive.5353@gmail.com>
2022-12-24 18:33:40 +0330

    Added init.lua

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Load the essentials
+require('core.options')
+require('core.mappings')
+
+-- Speed up nvim start time
+require('impatient')
+
+-- Load pywal colors'
+require('plugins.configs.pywal')
+
+-- Load packer plugings
+require('plugins')
